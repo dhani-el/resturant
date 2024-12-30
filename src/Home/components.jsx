8bef7b3794d47c4951c82f3222ef2fce67d4b1f8
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-12-30 23:37:50 +0100

    fix wierd landscape issue with the attraction component

--- src/Home/components.jsx
+++ src/Home/components.jsx
@@ -73,8 +73,8 @@ export function Header(){
         setOpenDrawer(()=>false);
     }
 
-    return <div className="z-10  flex w-full gap-10 lg:gap-0 lg:justify-between py-8 lg:py-4 items-center overflow-x-hidden">
-                <HambergerMenu onClick={openDrawer} className="lg:hidden" />
+    return <div className="z-10  flex w-full gap-10 xl:gap-0 xl:justify-between py-8 xl:py-4 items-center overflow-x-hidden">
+                <HambergerMenu onClick={openDrawer} className="xl:hidden" />
                 <Logo/>
                 <NavLinks links={navLinks}/>
                 <DrawerNav links={navLinks} shouldOpen={OpenDrawer} closeFunction={closeDrawer}/>
@@ -82,11 +82,11 @@ export function Header(){
 }
 export function Logo(){
     return <div className=" text-yellow-300 ">
-                    <p className="font-blacksword text-2xl lg:text-4xl">Steakhouse</p>
+                    <p className="font-blacksword text-2xl xl:text-4xl">Steakhouse</p>
     </div>
 }
 export function NavLinks({links=[]}){
-    return <div className="hidden lg:flex w-[50%] justify-between">
+    return <div className="hidden xl:flex w-[50%] justify-between">
                     {
                         links.map(link=>{
                             return <a href={link.url} className="text-sm font-inter font-normal">{link.title}</a>
@@ -95,7 +95,7 @@ export function NavLinks({links=[]}){
     </div>
 }
 function DrawerNav({links=[],shouldOpen,closeFunction}){
-    return <div className={`overflow-hidden p-4 w-screen h-screen absolute top-0 ${shouldOpen ?"left-[0rem]":"left-[110vw]"} bg-slate-950 font-noto text-2xl font-semibold z-50 flex flex-col justify-evenly items-center lg:hidden`}>
+    return <div className={`overflow-hidden p-4 w-screen h-screen absolute top-0 ${shouldOpen ?"left-[0rem]":"left-[110vw]"} bg-slate-950 font-noto text-2xl font-semibold z-50 flex flex-col justify-evenly items-center xl:hidden`}>
                 <div onClick={closeFunction} className="self-end flex text-yellow-300 gap-2 items-center font-inter">
                    <p>CLOSE </p> <CloseCircle className="w-[2.5rem] h-[2.5rem] self-end"/>
                 </div>
@@ -107,13 +107,13 @@ function DrawerNav({links=[],shouldOpen,closeFunction}){
     </div>
 }
 export function Attraction(){
-    return <div id="attraction" className="z-0 w-full h-fit justify-center landscape:h-[80vh] flex flex-col lg:flex-row lg:justify-start items-center ">
+    return <div id="attraction" className="z-0 w-full h-fit justify-center xl:h-[80vh] flex flex-col xl:flex-row xl:justify-start items-center ">
                     <AttractionText/>
                     <AttractionImage image={steakDish1} />
     </div>
 }
 function AttractionText(){
-    return <div className=" flex flex-col justify-start items-center lg:items-start w-full lg:w-[40%] gap-8 ">
+    return <div className=" flex flex-col justify-start items-center xl:items-start w-full xl:w-[40%] gap-8 ">
                 <p className="text-3xl md:text-6xl font-extrabold font-noto">
                     A Premium And Authentic Steakhouse
                 </p>
@@ -141,12 +141,12 @@ function AttractionImage({image}){
     })
 
     return <div id="attraction" className="pt-8 md:pt-0 w-full md:w-[60%] md:h-full flex md:flex-row relative justify-center items-center ">
-                <img id="wine" src={tomatospoon} className=" z-10 absolute top-[6%] h-[60%] md:h-[65%] right-[-30%] md:right-[-55%] lg:top-[-2%] lg:rotate-[20deg] lg:right-[10%] xl:right-[-5%]"/>
-                <img id="wine" src={herb1} className=" z-10 absolute h-[30%] md:h-[30%] right-4 md:right-[-15%] lg:right-[25%] lg:rotate-[20deg] "/>
-                <img id="wine" src={salt} className=" z-0 absolute h-[70%] md:h-[65%] right-0 md:right-[-15%] lg:right-[45%] lg:top-0"/>
-                <img id="wine" src={blackpepper} className=" z-[5] absolute h-[50%] md:h-[35%] right-[35%] md:right-[45%] lg:right-[55%] lg:bottom-28"/>
+                <img id="wine" src={tomatospoon} className=" z-10 absolute top-[6%] h-[60%] md:h-[65%] right-[-30%] md:right-[-55%] xl:top-[-2%] xl:rotate-[20deg] xl:right-[10%] xl:right-[-5%]"/>
+                <img id="wine" src={herb1} className=" z-10 absolute h-[30%] md:h-[30%] right-4 md:right-[-15%] xl:right-[25%] xl:rotate-[20deg] "/>
+                <img id="wine" src={salt} className=" z-0 absolute h-[70%] md:h-[65%] right-0 md:right-[-15%]  xl:right-[45%] xl:top-0"/>
+                <img id="wine" src={blackpepper} className=" z-[5] absolute h-[50%] md:h-[35%] right-[35%] md:right-[45%] xl:right-[55%] xl:bottom-28"/>
                 <img id="topleft" src={topleftImg} className=" z-10 absolute h-[30%] left-0 top-0 md:h-[30%]  md:left-[-10%] md:top-0"/>
-                <img id="bottom" src={bottom} className=" z-10 absolute h-[30%] md:h-[30%] left-0 bottom-[10%] md:left-[-20%] lg:left-[-12%] md:bottom-[20%]"/>
+                <img id="bottom" src={bottom} className=" z-10 absolute h-[30%] md:h-[30%] left-0 bottom-[10%] md:left-[-20%] xl:left-[-12%] md:bottom-[20%]"/>
                 <div className=" z-10 w-full md:w-[90%] md:h-[90%] flex items-center justify-center md:justify-start ">
                     <img id = "pan" src={image}  className="w-[60%] h-auto md:h-full md:w-auto rotate-[160deg] "/>
                 </div>
@@ -166,11 +166,11 @@ export function OurStory(){
         })
     })
 
-    return <div id="ourStory" className="z-0 w-full  pt-16 lg:pt-0 lg:h-screen relative flex items-center justify-center">
-                <img id="topsalt" src={salt2} className="absolute w-[55%] lg:w-[18%] z-0 top-[4%] rotate-45 left-[-5%] md:top-[2%] md:left-[3rem] lg:top-[6%] lg:left-6" />
-                <div className="w-full h-full md:w-[70%] lg:w-[85%] lg:h-[70%] flex flex-col-reverse lg:flex-row z-10 relative ">
-                    <img src={our_story_dish} className="w-[100%] lg:h-[100%] lg:w-auto "  />
-                    <div className="lg:w-[50%] bg-white relative text-black p-8 md:p-16 flex flex-col gap-4 justify-center items-start">
+    return <div id="ourStory" className="z-0 w-full  pt-16 xl:pt-0 xl:h-screen relative flex items-center justify-center">
+                <img id="topsalt" src={salt2} className="absolute w-[55%] xl:w-[18%] z-0 top-[4%] rotate-45 left-[-5%] md:top-[2%] md:left-[3rem] xl:top-[6%] xl:left-6" />
+                <div className="w-full h-full md:w-[70%] xl:w-[85%] xl:h-[70%] flex flex-col-reverse xl:flex-row z-10 relative ">
+                    <img src={our_story_dish} className="w-[100%] xl:h-[100%] xl:w-auto "  />
+                    <div className="xl:w-[50%] bg-white relative text-black p-8 md:p-16 flex flex-col gap-4 justify-center items-start">
                         <div>
                             <p className="font-bonheur text-orange-300 text-2xl">Discover</p>
                             <p className="font-noto text-4xl font-extrabold">Our Story</p>
@@ -182,17 +182,17 @@ export function OurStory(){
                         <img src={herb2} className="h-[40%] absolute bottom-[-10%] right-[-8%] rotate-[45deg]"/>
                     </div>
                 </div>
-                <img src={salt}  className="absolute w-[75%] lg:w-[35%] z-0 bottom-[-5%] lg:bottom-[5%] left-[-15%] lg:left-[10%] rotate-90" />
+                <img src={salt}  className="absolute w-[75%] xl:w-[35%] z-0 bottom-[-5%] xl:bottom-[5%] left-[-15%] xl:left-[10%] rotate-90" />
     </div>
 }
 export function OurMenu(){
     return <div className="z-0 w-full flex flex-col items-center">
-                <div className="flex flex-col w-full lg:flex-row lg:w-[50%] gap-6 lg:gap-12 items-center">
-                    <div className="w-full items-center lg:w-[40%] flex flex-col lg:items-end">
+                <div className="flex flex-col w-full xl:flex-row xl:w-[50%] gap-6 xl:gap-12 items-center">
+                    <div className="w-full items-center xl:w-[40%] flex flex-col xl:items-end">
                         <p className="font-bonheur text-xl text-orange-300 ">Discover</p>
                         <p className="font-noto text-3xl font-extrabold">Our Menu</p>
                     </div>
-                    <div className="md:w-[80%] md:text-center lg:w-[60%]">
+                    <div className="md:w-[80%] md:text-center xl:w-[60%]">
                         <p className="text-[0.6rem] font-inter font-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad adipisci corrupti totam, nobis alias reprehenderit delectus error dolores! Voluptatem, ipsum accusamus. Recusandae quod molestiae aliquam.</p>
                     </div>
                 </div>
@@ -219,14 +219,14 @@ function ADish({direction,title,summary,image,id}){
         })
     },[id])
 
-    return <div className={`px-0 md:px-8 lg:px-0 pb-6 flex ${(direction === "rev") ? "lg:flex-row-reverse flex-col":"flex-col lg:flex-row"} gap-9 md:gap-0 lg:gap-16 items-center lg:w-[70%] `}>
-                <div className={`w-[75%] md:w-[50%] lg:w-[35%] ${(direction === "rev") ? "self-end":"self-start"} lg:self-center relative`}>
+    return <div className={`px-0 md:px-8 xl:px-0 pb-6 flex ${(direction === "rev") ? "xl:flex-row-reverse flex-col":"flex-col xl:flex-row"} gap-9 md:gap-0 xl:gap-16 items-center xl:w-[70%] `}>
+                <div className={`w-[75%] md:w-[50%] xl:w-[35%] ${(direction === "rev") ? "self-end":"self-start"} xl:self-center relative`}>
                     <img src={(direction === "rev") ? tomato : herb1} className={`absolute ${(direction === "rev") ?"w-[20%] bottom-0 right-0":"w-[28%] bottom-0 left-[-5%] rotate-[310deg]"} `}/>
-                    <img src={salt3} className={`absolute w-[200%] h-auto   ${(direction === "rev") ? " left-0 lg:left-[-70%] bottom-[-15%] lg:bottom-[-40%] rotate-[155deg]":"left-0 lg:left-[70%] bottom-[-15%] lg:bottom-[-50%] rotate-45"}`}/>
+                    <img src={salt3} className={`absolute w-[200%] h-auto   ${(direction === "rev") ? " left-0 xl:left-[-70%] bottom-[-15%] xl:bottom-[-40%] rotate-[155deg]":"left-0 xl:left-[70%] bottom-[-15%] xl:bottom-[-50%] rotate-45"}`}/>
                     <img src={salt2} className="absolute w-[120%] h-[100%] rotate-45  "/>
                     <img id={`${id}`} src={image} className={`w-[100%]`}/>
                 </div>
-                <div className={` z-10 px-0 md:px-4 lg:px-0 w-[75%] ${(direction === "rev") ? "self-start":"self-end"} lg:self-center md:w-[50%] lg:w-[40%]`}>
+                <div className={` z-10 px-0 md:px-4 xl:px-0 w-[75%] ${(direction === "rev") ? "self-start":"self-end"} xl:self-center md:w-[50%] xl:w-[40%]`}>
                     <p className="font-bonheur text-3xl text-orange-300 m-0">{title}</p>
                     <p className="text-[0.6rem] font-inter font-light">{summary || "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad adipisci corrupti totam, nobis alias reprehenderit delectus error dolores! Voluptatem, ipsum accusamus. Recusandae quod molestiae aliquam."}</p>
                 </div>
@@ -256,11 +256,11 @@ export function UpcomingEvents(){
         })
     })
 
-    return <div className="z-0 w-full  pt-16 lg:pt-0 lg:h-screen relative flex items-center justify-center">
-                <img id ="topsaltcoming" src={salt} className="absolute w-[55%] lg:w-[18%] z-0 top-[4%] left-[-5%] md:top-[2%] md:left-[3rem] lg:top-[8%] lg:left-6" />
-                <div className="w-full h-full md:w-[70%] lg:w-[85%] lg:h-[70%] flex flex-col-reverse lg:flex-row z-10 relative">
-                    <img src={our_story_dish} className="w-[100%] lg:h-[100%] lg:w-auto "  />
-                    <div className="lg:w-[50%] bg-white relative text-black p-8 md:p-16 flex flex-col gap-4 justify-center items-start">
+    return <div className="z-0 w-full  pt-16 xl:pt-0 xl:h-screen relative flex items-center justify-center">
+                <img id ="topsaltcoming" src={salt} className="absolute w-[55%] xl:w-[18%] z-0 top-[4%] left-[-5%] md:top-[2%] md:left-[3rem] xl:top-[8%] xl:left-6" />
+                <div className="w-full h-full md:w-[70%] xl:w-[85%] xl:h-[70%] flex flex-col-reverse xl:flex-row z-10 relative">
+                    <img src={our_story_dish} className="w-[100%] xl:h-[100%] xl:w-auto "  />
+                    <div className="xl:w-[50%] bg-white relative text-black p-8 md:p-16 flex flex-col gap-4 justify-center items-start">
                         <div>
                             <p className="font-bonheur text-orange-300 text-2xl">Discover</p>
                             <p className="font-noto text-4xl font-extrabold">Upcoming Events</p>
@@ -276,7 +276,7 @@ export function UpcomingEvents(){
                         <img src={spoon} className="h-[40%] absolute bottom-[-20%] right-[-20%] rotate-[245deg]"/>
                     </div>
                 </div>
-                <img src={salt2} id="bottomsaltcoming" className="absolute w-[65%] bottom-[-8%] lg:w-[40%] z-0 lg:bottom-[8%] left-[10%] rotate-90" />
+                <img src={salt2} id="bottomsaltcoming" className="absolute w-[65%] bottom-[-8%] xl:w-[40%] z-0 xl:bottom-[8%] left-[10%] rotate-90" />
 
     </div>
 }
@@ -296,13 +296,13 @@ export function BestIngredients(){
         })
     })
 
-    return <div  className="flex flex-col items-center w-full lg:h-screen">
+    return <div  className="flex flex-col items-center w-full xl:h-screen">
                         <div className="flex flex-col items-center md:w-[70%]">
                             <p className="font-bonheur text-orange-300 text-2xl">Discover</p>
                             <p className="font-noto text-3xl md:text-4xl font-extrabold">The Best Ingredients</p>
                             <p className=" pt-4 font-inter text-sm md:text-base font-extralight text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, error tempore delectus earum officia ipsam, tempora mollitia minus ratione reprehenderit praesentium numquam deleniti inventore reiciendis molestias commodi non sint sapiente? Repudiandae deleniti perferendis ullam ex!</p>
                         </div>
-                        <img id="ingredientsImage" src={ingredients} className="w-[96%] md:w-[90%] h-auto lg:w-auto lg:h-[62%] pt-6"/>
+                        <img id="ingredientsImage" src={ingredients} className="w-[96%] md:w-[90%] h-auto xl:w-auto xl:h-[62%] pt-6"/>
     </div>
 }
 export function Reservation(){
@@ -319,7 +319,7 @@ export function Reservation(){
             ease:"expo.inOut"
         })
     })
-    return <div className="w-full lg:h-[70vh] overflow-hidden relative">
+    return <div className="w-full xl:h-[70vh] overflow-hidden relative">
                 <img id="resturantImage" className="w-full max-w-[100%] z-0 " src={fillinresturant} />
                 <div className="w-full h-full absolute top-0 left-0 z-10 bg-black opacity-30">
 
